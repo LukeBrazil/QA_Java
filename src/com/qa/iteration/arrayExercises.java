65b474b57117a979d3c99a15899901b44386e97a
lukebrazil <luke.brazil13@icloud.com>
2021-05-17 16:32:26 -0500

    array exercises

---
+++ src/com/qa/iteration/arrayExercises.java
@@ -0,0 +1,38 @@
+package com.qa.iteration;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class arrayExercises {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		
+		for (int i = 0; i < nums.length; i++) {
+			System.out.println(nums[i]);
+		}
+		
+
+		ArrayList<Integer> fillerArray = new ArrayList<Integer>();		
+		
+		for (int i = 0; i <= 10; i++) {
+			fillerArray.add(i, i);
+			System.out.println(fillerArray.get(i));
+		}
+		for (int num : fillerArray) {
+			num = num * 10;
+			System.out.println(num);
+		}
+		
+		for (int i = 0; i < fillerArray.size(); i++) {
+			fillerArray.set(i, fillerArray.get(i) * 10);
+		}
+		System.out.println(">>>> " + fillerArray.toString());
+		
+	}
+	
+	
+
+}
