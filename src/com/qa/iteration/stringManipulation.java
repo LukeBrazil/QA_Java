751f7f03cc3a376ce8c9c5bb6ccde3d35cc58abe
lukebrazil <luke.brazil13@icloud.com>
2021-05-18 11:40:21 -0500

    finished string exercises

--- src/com/qa/iteration/stringManipulation.java
+++ src/com/qa/iteration/stringManipulation.java
@@ -7,30 +7,33 @@ public class stringManipulation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		Create two Strings where one string has a value of “yesterday it was raining” and the other string with a value of “today it is sunny”
-		String x = "Today it was Sunny";
-		String y = "Yesterday it is raining.";
+		String x = "Today it is Sunny";
+		String y = "Yesterday it was raining.";
 //		Concatenate both values, capitalise both strings and print out the result.
 		String xy = x.toUpperCase() + ", " + y.toUpperCase();
+		System.out.println(xy);
 		// Now use the substring method to print out: `TODAY IT IS RAINING`
-		String substring = xy.substring(0, 6) + xy.substring(30, 44);
+		String substring = xy.substring(0, 11) + xy.substring(35, 43);
 		System.out.println(substring);
+		
 		// Method 1 - When given a String, count and return how many words there are in that String.
 		String st1 = "The wind is howling outside!";
-		String[] split = st1.split(" ", 6);
+		String[] split = st1.split(" ", st1.length());
 		System.out.println(Arrays.toString(split));
-		System.out.println(split.length);
+	
 
 		
-		String st2 = "The cold wind blows";
 		// Method 2 - When given a String, print out this String in a vertical fashion, each word on a different line.
 		for (int i = 0; i < split.length; i++) {
 			System.out.println(split[i]);
 		}
+		System.out.println(" ");
 		
 		// Method 3 - When given a String, print out this String in a vertical fashion in reverse order, each word on a different line.
 		for (int i = split.length - 1; i >= 0; i--) {
 			System.out.println(split[i]);
 		}
+		System.out.println(" ");
 		
 		// Method 4 - A find method, which takes 2 Strings; the first is message and the second is the String you want to find in the message. A boolean value should be returned to indicate whether or not the second String has been found in the message.
 		String message = "The winds whispers mary.";
